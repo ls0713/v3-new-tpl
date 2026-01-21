367f7a51859afdf3c87fd98da60971966d3977ad
Liu <mrliu0713@163.com>
2026-01-21 11:41:47 +0800

    setup pinia is function

--- src/app/setup.ts
+++ src/app/setup.ts
@@ -5,6 +5,6 @@ import { setupPinia } from './pinia'
 import { setupRouter } from './router'
 
 export async function setupApp (app: App) {
-  app.use(setupPinia)
+  app.use(setupPinia())
   await setupRouter(app)
 }
